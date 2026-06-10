4be8f62858615a0662b01f70e61e260e51f74684
sit23ec003-maker <sit23ec003@sairamtap.edu.in>
2026-06-10 15:02:11 +0530

    Update predict.py

--- src/src/src/predict.py
+++ src/src/src/predict.py
@@ -1,1 +1,16 @@
+print("AI Modulation Classifier")
 
+feature1=float(input("Feature1: "))
+feature2=float(input("Feature2: "))
+
+if feature1 < 1.5:
+    print("BPSK")
+
+elif feature1 < 2.5:
+    print("QPSK")
+
+elif feature1 < 3.5:
+    print("16QAM")
+
+else:
+    print("64QAM")
